2946b2929a91319ab4c582a4fd368356c7d2a550
ufrutov <mailluifruti@mail.ru>
2017-11-25 23:51:46 +0200

    Commit Kode ex 2 - app with photo caption

---
+++ kodeex2/app/src/main/java/com/ex/kode/kodeex2/CameraActivity.kt
@@ -0,0 +1,51 @@
+package com.ex.kode.kodeex2
+
+import android.content.Intent
+import android.graphics.Bitmap
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import java.io.IOException
+
+class CameraActivity : AppCompatActivity() {
+
+    lateinit var img_caption: TextView
+    lateinit var photo_view: ImageView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_camera)
+
+        img_caption = findViewById(R.id.caption)
+
+        var text_caption = intent.getStringExtra("caption")
+
+        if( text_caption.length > 0 )
+            img_caption.text = text_caption
+        else {
+            img_caption.text = "Caption Error"
+            Toast.makeText(applicationContext, "Photo caption error", Toast.LENGTH_SHORT).show()
+        }
+
+        photo_view = findViewById(R.id.img)
+
+        try {
+            var photo = intent.getParcelableExtra<Bitmap>("photo") as Bitmap
+
+            photo_view.setImageBitmap(photo)
+        } catch(e: IOException) {
+            Toast.makeText(applicationContext, "Photo file error", Toast.LENGTH_SHORT).show()
+        }
+
+//        Return to Main activity
+        var back_btn: Button = findViewById(R.id.btn_back)
+
+        back_btn.setOnClickListener(View.OnClickListener {
+            finish()
+        })
+    }
+}
---
+++ kodeex2/app/src/main/java/com/ex/kode/kodeex2/MainActivity.kt
@@ -0,0 +1,67 @@
+package com.ex.kode.kodeex2
+
+import android.app.Activity
+import android.content.Intent
+import android.graphics.Bitmap
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var caption_input: EditText
+
+    val REQUEST_IMAGE_CAPTURE = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var caption_input: EditText = findViewById(R.id.img_caption)
+        caption_input.setText("")
+
+        // Button handler
+        var startBtn: Button = findViewById(R.id.img_proceed)
+        startBtn.setOnClickListener(View.OnClickListener {
+
+            var caption: String = caption_input.text.toString()
+
+            if (caption.length < 1)
+                Toast.makeText(applicationContext, R.string.error, Toast.LENGTH_SHORT).show()
+            else {
+                // Start next activity with phone camera
+
+                var takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+
+                if (takePictureIntent.resolveActivity(getPackageManager()) != null)
+                    startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+            }
+        })
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == Activity.RESULT_OK) {
+            var photo: Bitmap = data.getExtras().get("data") as Bitmap
+
+            if( photo == null )
+                Toast.makeText(applicationContext, "NULL Photo", Toast.LENGTH_SHORT).show()
+
+            var intent = Intent(applicationContext, CameraActivity::class.java)
+
+            var caption_input: EditText = findViewById(R.id.img_caption)
+            var caption: String = caption_input.text.toString()
+
+            intent.putExtra("caption", caption)
+            intent.putExtra("photo", photo)
+
+            startActivity(intent)
+        } else
+            Toast.makeText(applicationContext, "Camera response error", Toast.LENGTH_SHORT).show()
+    }
+}
